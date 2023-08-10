be7ecc3f25375ef103b49e7ac44ae0baa84e716f
hammadkhanwazir <insafstudentpti@gmail.com>
2023-08-10 08:48:50 +0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
